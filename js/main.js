4713a7f493bc5e8a8b4870cec4de4b28878ab25a
garethnicholas <gnicholas@yahoo-inc.com>
2016-11-07 18:59:00 +0000

    sync

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+// ***** BURGER MENU ***** //
+
 console.log('it works');
+
+$(document).ready(function(){
+
+	var navList = $('.menu' 'ul');
+	
+	$('.nav-icon').on('click', function(){
+		navList.slideToggle();
+	});
+	$(window).on('resize', function(){
+
+		if ( $(window).width() > 3000 ) {
+			navList.show();
+		} else {
+			navList.hide();
+		}
+	});
+});
